cd3622497c2295b5254fa6d5c725d91920bcdd14
Mehedi Hasan Shanto <shantoahammed44@gmail.com>
2025-12-03 19:33:16 +0600

    Update python_practice.py

--- python_practice.py
+++ python_practice.py
@@ -27,3 +27,41 @@ print(even_numbers)
 
 
 #3. Count word frequency in a sentence
+def word_frequency(sentence):
+    words = sentence.split() #splitting the sentence ["I", "love", "python", "I", "love", "coding"]
+    freq = {}
+    for word in words:
+        if word in freq:
+            freq[word] += 1
+        else:
+            freq[word] = 1
+    return freq
+
+sentence = input("Enter a sentence: ")
+print(word_frequency(sentence))
+
+
+#4. Reverse a string
+def reverse(text):
+    string=text[::-1]
+    return string
+
+name = input("Enter your name: ")
+rev = reverse(name)
+print(rev)
+
+
+#5. Find the max value in a dictionary
+def max_in_dict(dict):
+    return max(dict.values())
+
+dict = {}
+n = int(input("how many items do you want: "))
+
+for i in range(n):
+    key = input("enter item: ")
+    value = input("enter value: ")
+    dict[key] = value
+
+max_value = max_in_dict(dict)
+print(max_value)
